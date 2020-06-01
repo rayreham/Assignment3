b42c39223e9699f71ae9520bd194ec51664bb520
rayreham <54294677+rayreham@users.noreply.github.com>
2020-06-01 16:14:15 -0400

    Update script.js

--- script.js
+++ script.js
@@ -30,4 +30,23 @@ let newEl = document.createElement("li");
 // 9 - Give the li the text "four".
 newEl.innerText = "four";
 
+//10 - Append the li to the ul elementlet newLi = document.createElement('li');
+newLi.textContent = 'four';
+let ul = document.querySelector('ul');
+ul.appendChild(newLi);
+
+//12 - Loop over all of the lis inside the ol tag and give them a background color of "green".
+
+let li = document.querySelectorAll('ol li');
+
+for(let i = 0; i <li.length; i++){
+    li[i].style.color = 'green';
+}
+
+//13 - Remove the div with a class of footer.
+
+let footer = document.getElementsByClassName('footer');
+footer[0].parentNode.removeChild(footer[0]);
+
+
 
