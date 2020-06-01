8b7f2e7afc93cbba341245a25e373516de79152d
Nicole <nwoje13@gmail.com>
2020-06-01 15:17:20 -0400

    added 7 to 9

---
+++ script.js
@@ -0,0 +1,8 @@
+// 7 - Remove the class main on the div with a class of footer. 
+footer.classList.remove("main");
+
+// 8 - Create a new li element.
+let newEl = document.createElement("li");
+
+// 9 - Give the li the text "four".
+newEl.innerText = "four";
