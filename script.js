75d8825b5f3c1fc9564e8d66973f0dc257a68328
PokeLegoCuber <PokeLegoCuber@yahoo.com>
2020-06-01 15:40:43 -0400

    pushing task 4-6

---
+++ script.js
@@ -0,0 +1,15 @@
+//4) Select a list item with a class of third, but only the list item inside of the ol tag.
+// let ol_third = document.querySelector("ol .third");
+// console.log(ol_third);
+
+//5) Give the section with an id of container the text "Hello!".
+// let loc = document.getElementById('container');
+// loc.innerHTML = "Hello!";
+
+//6) Add the class main to the div with a class of footer.
+// let new_element = document.getElementsByClassName('footer');
+// new_element.classList = "main";
+//This should be the soutlion but it gives me errors
+// new_element.classList.add("main");
+
+
